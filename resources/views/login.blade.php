3426ed8f622e0063b4f594a22d7128a6953eebb6
sreenathzs <sreenath@zelarsoft.com>
2025-01-03 12:18:00 +0530

    create a sample larval app

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form>
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
